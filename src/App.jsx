7cb579319964d1a7131aa569db506a3dd5fe49ed
Shehan <kurugamaperera2003@gmail.com>
2025-01-31 00:07:50 +0530

    init ⭐

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import './App.css'
+import ProductCard from './components/productCard'
+
+function App() {
+
+  return (
+    <>
+      <ProductCard name="Audio Setup" price="3250/-" description="Meow Bark Meow Bark Meow Bark" img="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR_MCjpCcNS9G7BRyYyCLfJMBk50v_5wThcTg&s"/>
+    
+      <ProductCard 
+      name="MiNI Game Player" 
+      price="2374/-" 
+      description="Summer Cartoon Cat Couple Home Shoes Non-slip Flat Slides Lithe Sandals For Women Men Slippers Male Indoor Flip Flops" 
+      img="https://img.drz.lazcdn.com/g/kf/Sac64f5a5c58e4bdda3052dfd89ba39c7A.jpg_200x200q80.jpg_.webp"/>
+      
+    </>
+  )
+}
+
+export default App
