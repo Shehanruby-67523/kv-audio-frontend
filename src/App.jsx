ba48fa5ca38468f85e768b16661d165c224ed206
Shehan <kurugamaperera2003@gmail.com>
2025-02-02 20:46:58 +0530

    refactor: move sidebar components to AdminPage for better structure ⭐

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,11 @@
 import './App.css'
-import { BsGraphDown } from "react-icons/bs";
-import { FaRegBookmark, FaRegUser } from "react-icons/fa";
-import { MdOutlineSpeaker } from "react-icons/md";
+import AdminPage from './components/adminPage';
 function App() {
 
   return (
     
-    <div className='w-full h-screen flex'>
-      <div className='w-[400px] h-full bg-green-200'>
-       <button className='w-full h-[40px] text-[25px] font-bold flex justify-center items-center'>
-       <BsGraphDown />
-        Dashboard
-        </button>
-        <button className='w-full h-[40px] text-[25px] font-bold flex justify-center items-center'>
-        <FaRegBookmark />
-        Bookings
-        </button>
-        <button className='w-full h-[40px] text-[25px] font-bold flex justify-center items-center'>
-        <MdOutlineSpeaker />
-        Items
-        </button>
-        <button className='w-full h-[40px] text-[25px] font-bold flex justify-center items-center'>
-        <FaRegUser />
-        Users
-        </button>
-      </div>
-      <div className='w-full bg-red-900'>
-      
-      </div>
+    <div>
+      <AdminPage/>
     </div>
   )
 }
