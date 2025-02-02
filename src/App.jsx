715f6be9981142a5711bed325db9587a234e27a3
Shehan <kurugamaperera2003@gmail.com>
2025-02-02 20:31:09 +0530

    Day 10

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,32 @@
 import './App.css'
-
+import { BsGraphDown } from "react-icons/bs";
+import { FaRegBookmark, FaRegUser } from "react-icons/fa";
+import { MdOutlineSpeaker } from "react-icons/md";
 function App() {
 
   return (
     
-    <div>
-      <div className='w-[800px] h-[800px] bg-yellow-200'>
-      <div className="w-[500px] h-[500px] bg-blue-200 flex flex-col justify-center items-center">
-        <div className='w-[50px] h-[50px] bg-red-500'>
-
-        </div>
-        <div className='w-[50px] h-[50px] bg-green-500'>
-
-        </div>
-        <div className='w-[50px] h-[50px] bg-yellow-500'>
-
-        </div>
+    <div className='w-full h-screen flex'>
+      <div className='w-[400px] h-full bg-green-200'>
+       <button className='w-full h-[40px] text-[25px] font-bold flex justify-center items-center'>
+       <BsGraphDown />
+        Dashboard
+        </button>
+        <button className='w-full h-[40px] text-[25px] font-bold flex justify-center items-center'>
+        <FaRegBookmark />
+        Bookings
+        </button>
+        <button className='w-full h-[40px] text-[25px] font-bold flex justify-center items-center'>
+        <MdOutlineSpeaker />
+        Items
+        </button>
+        <button className='w-full h-[40px] text-[25px] font-bold flex justify-center items-center'>
+        <FaRegUser />
+        Users
+        </button>
       </div>
+      <div className='w-full bg-red-900'>
+      
       </div>
     </div>
   )
