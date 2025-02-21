05fb29af2d5b7e25f02aeffbb859f3fecdb4f7ac
Shehan <kurugamaperera2003@gmail.com>
2025-02-21 11:27:11 +0530

    added post request to the handleOnSubmit function 🎈

--- src/pages/login/login.jsx
+++ src/pages/login/login.jsx
@@ -1,20 +1,43 @@
+import axios from "axios";
 import "./login.css"
 import { useState } from "react"
 
 export default function LoginPage() {    
-const[email, setEmail] = useState("")
-const[password, setPassword] = useState("")
+const[email, setEmail] = useState("");
+const[password, setPassword] = useState("");
+
+function handleOnSubmit(e) {
+    console.log("submitted")
+    e.preventDefault()
+    console.log(email, password)
+
+    axios.post("http://localhost:3000/api/users/login")
+}
 
     return (    
-        <div className="bg-picture w-full h-screen flex justify-center items-center">    
+        <div className="bg-picture w-full h-screen flex justify-center items-center">  
+            <form onSubmit={handleOnSubmit}>
             <div className="w-[400px] h-[400px] backdrop-blur-xl rounded-2xl flex justify-center items-center flex-col relative">
                 <img src="/logo.png" alt="logo" className="w-[100px] h-[100px] object-cover absolute top-10"/>
                 
-                <input type="email" placeholder="Email" className="w-[300px] h-[30px] mt-6 bg-transparent border-b-2 border-white text-white text-xl outline-none"/>
+                <input type="email" placeholder="Email" className="w-[300px] h-[30px] mt-6 bg-transparent border-b-2 border-white text-white text-xl outline-none" 
+                value={email}
+                onChange={
+                    (e) => {
+                        setEmail(e.target.value);
+                        }   
+                    }/>
                 
-                <input type="password" placeholder="Password" className="w-[300px] h-[30px] mt-6 bg-transparent border-b-2 border-white text-white text-xl outline-none"/>
+                <input type="password" placeholder="Password" className="w-[300px] h-[30px] mt-6 bg-transparent border-b-2 border-white text-white text-xl outline-none"
+                value={password}
+                onChange={
+                    (e) => {
+                        setPassword(e.target.value);
+                    }
+                }/>
                 <button className="my-8 w-[300px] h-[40px] bg-[#efac38] text-white text-2xl rounded-lg">Login</button>
             </div>
+            </form>  
         </div>
     )
 }
